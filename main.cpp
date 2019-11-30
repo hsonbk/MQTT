e841fbe22ba9a8b37501acb661e20c20144e11be
hsonbk <sondhust@gmail.com>
2019-11-30 11:37:40 +0700

    edit interrupt

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ const char *host = "test.mosquitto.org";
 int port = 1883;
 std::string mess;
 
+myMosquitto* mqtt;
+
 void signalHandle(int signum){
     std::cout << " Catch signal " << signum << std::endl << "Exit program" << std::endl;
     exit(signum);
@@ -16,7 +18,7 @@ void signalHandle(int signum){
 
 int main(int argc, char *argv[])
 {   
-    myMosquitto* mqtt;
+    // myMosquitto* mqtt;
     signal(SIGINT, signalHandle);
     while(1){
         mqtt = new myMosquitto(id, host, port);
@@ -27,15 +29,13 @@ int main(int argc, char *argv[])
         sleep(2);
         std::cout << "Enter message: ";
         std::getline(std::cin,mess);
-        // std::cout << "Enter message: ";
-        // std::getline(std::cin,mess);
         mqtt->myPublish(_topic, mess);
         sleep(2); 
         //mqtt->unsubscribe_from_topic(_topic);
         // std::cout << "About to delete mqtt" << std::endl;
         //delete mqtt;
         // std::cout << "Mqtt deleted" << std::endl;
-        sleep(3);
+        sleep(2);
         }
     return 0;
 }
